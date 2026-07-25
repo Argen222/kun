cece2289a6184f8aa1302656dd23f266e4845727
Argen222 <azamatovargen12@gmail.com>
2026-07-25 13:52:24 +0600

    test

--- src/app/components/LithophanyConstructor.jsx
+++ src/app/components/LithophanyConstructor.jsx
@@ -256,12 +256,12 @@ function LithophanyLamp({
     capTop.position.y = H / 2 - CAP_OVERLAP + CAP_H + Y_OFFSET;
     group.add(capTop);
 
-    group.position.y = 1.0;
+    group.position.y = 1.2;
 
     const floorMat = new THREE.MeshStandardMaterial({ color: 0x16181d, roughness: 0.85, metalness: 0.1 });
     const floor = new THREE.Mesh(new THREE.CircleGeometry(40, 64), floorMat);
     floor.rotation.x = -Math.PI / 2;
-    floor.position.y = -H / 2 + group.position.y - 1.0;
+    floor.position.y = -H / 2 + group.position.y - 1.2;
     scene.add(floor);
 
     const poolMat = new THREE.MeshBasicMaterial({
